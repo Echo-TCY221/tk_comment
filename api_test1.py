fd41e9e55c1eb9266fc629b496c11df22ce9738d
tang_cy <t2124642771@163.com>
2024-08-13 16:56:33 +0800

    2024/08/13

--- api_test1.py
+++ api_test1.py
@@ -2,13 +2,13 @@ import json
 
 import requests
 
-url = "https://cn-sy-bgp-plustmp1.natfrp.cloud:19754/tk_search"
+# url = "https://cn-sy-bgp-plustmp1.natfrp.cloud:19754/tk_search"
 # url = "http://192.168.1.7:8899/tk_search"
-# url = "http://127.0.0.1:8901/tk_user_search"
+url = "http://127.0.0.1:8901/tk_user_search"
 
 data_1 = {
-    "key_worlds": "小兜包",
-    "page": 3
+    "key_worlds": "83780769066",
+    # "page": 1
 }
 
 data = {
